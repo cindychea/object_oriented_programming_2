552edcf2ef2af7fccc5baa82ebc8a39e698a3ece
cindychea <cindy_chea@hotmail.com>
2019-06-16 21:51:28 -0400

    edited file using black

--- book_lending.py
+++ book_lending.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import random
 
+
 class Book:
 
     on_shelf = []
@@ -12,13 +13,13 @@ class Book:
         self.author = author
         self.ISBN = ISBN
         self.due_date = None
-    
+
     @classmethod
     def create(cls, title, author, ISBN):
         book = Book(title, author, ISBN)
         cls.on_shelf.append(book)
         return book
-    
+
     @classmethod
     def browse(cls):
         return random.choice(cls.on_shelf)
@@ -26,7 +27,7 @@ class Book:
     @classmethod
     def current_due_date(cls):
         now = datetime.now()
-        two_weeks = 60 * 60 * 24 * 14 # two weeks expressed in seconds  
+        two_weeks = 60 * 60 * 24 * 14  # two weeks expressed in seconds
         future_timestamp = now.timestamp() + two_weeks
         return datetime.fromtimestamp(future_timestamp)
 
@@ -39,7 +40,7 @@ class Book:
     def borrow(self):
         if self.lent_out():
             return False
-        else: 
+        else:
             Book.on_shelf.remove(self)
             Book.on_loan.append(self)
             self.due_date = Book.current_due_date()
@@ -51,7 +52,7 @@ class Book:
             Book.on_shelf.append(self)
             self.due_date = None
             return True
-        else: 
+        else:
             return False
 
     @classmethod
@@ -60,23 +61,42 @@ class Book:
             if Book.current_due_date() < datetime.now():
                 cls.overdue_books.append(book)
 
+
 sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
 aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
-if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
-print(Book.browse().title) # "Sister Outsider" (this value may be different for you)
-print(Book.browse().title) # "Ain't I a Woman?" (this value may be different for you)
-print(len(Book.on_shelf)) # 3
-print(len(Book.on_loan)) # 0
-print(sister_outsider.lent_out()) # False
-print(sister_outsider.borrow()) # True
-print(len(Book.on_shelf)) # 2
-print(len(Book.on_loan)) # 1
-print(sister_outsider.lent_out()) # True
-print(sister_outsider.borrow()) # False
-print(sister_outsider.due_date) # 2017-02-25 20:52:20 -0500 (this value will be different for you)
+if_they_come = Book.create(
+    "If They Come in the Morning", "Angela Y. Davis", "0893880221"
+)
+print(Book.browse().title)
+# "Sister Outsider" (this value may be different for you)
+print(Book.browse().title)
+# "Ain't I a Woman?" (this value may be different for you)
+print(len(Book.on_shelf))
+# 3
+print(len(Book.on_loan))
+# 0
+print(sister_outsider.lent_out())
+# False
+print(sister_outsider.borrow())
+# True
+print(len(Book.on_shelf))
+# 2
+print(len(Book.on_loan))
+# 1
+print(sister_outsider.lent_out())
+# True
+print(sister_outsider.borrow())
+# False
+print(sister_outsider.due_date)
+# 2017-02-25 20:52:20 -0500 (this value will be different for you)
 print(Book.overdue_books)
-print(len(Book.overdue_books)) # 0
-print(sister_outsider.return_to_library()) # True
-print(sister_outsider.lent_out()) # False
-print(len(Book.on_shelf)) # 2
-print(len(Book.on_loan)) # 0
+print(len(Book.overdue_books))
+# 0
+print(sister_outsider.return_to_library())
+# True
+print(sister_outsider.lent_out())
+# False
+print(len(Book.on_shelf))
+# 2
+print(len(Book.on_loan))
+# 0
